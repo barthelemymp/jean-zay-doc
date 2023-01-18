40dcf9528d62fcb6e456779ea2509e1c299ab6c3
barthelemymp <barthelemy.meynard@polytechnique.edu>
2023-01-18 22:40:05 +0100

    test

--- docs/examples/pytorch/mnist/pytorch_example_script.sh
+++ docs/examples/pytorch/mnist/pytorch_example_script.sh
@@ -14,11 +14,10 @@ set -x
 cd ${SLURM_SUBMIT_DIR}
 export WANDB_MODE="offline"
 module purge
-module load pytorch-gpu/py3/1.11.0
+
 module load anaconda-py3/2021.05
-
 conda activate /gpfswork/rech/mdb/urz96ze/miniconda3/envs/Barth
-
+module load pytorch-gpu/py3/1.11.0
 ###module load pytorch-gpu/py3/1.4.0 
 
 python ./mnist_example.py 
